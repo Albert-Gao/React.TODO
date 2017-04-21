8c273630508a5f019f29256f7d9c675dc392e8d3
Albert Gao <albertgaohy@gmail.com>
2017-04-22 04:28:26 +1200

    Try something.

--- app/app.js
+++ app/app.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom'
 
 //TODO: Add Webpack later.
 
-const Hello = (
-    <h1>Hello React !!!!</h1>
+const Greeting = (
+    <h2>I feel good!</h2>
 );
 
+class First extends React.Component{
+    render(){
+        return(
+            <div>
+                {Greeting},
+                <h1>Hello {this.props.myname}</h1>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    Hello,
+    <First myname="Albert!"/>,
     document.getElementById('root')
 )
