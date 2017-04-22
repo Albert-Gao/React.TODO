9ea2db9cbec6a577cc0831624737ce8dd9b18745
Albert Gao <albertgaohy@gmail.com>
2017-04-23 00:09:27 +1200

    Decouple one component
    Signed-off-by: Albert Gao <albertgaohy@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {First} from './Components'
 
-//TODO: Add Webpack later.
-
-const Greeting = (
-    <h2>I feel good!</h2>
-);
-
-class First extends React.Component{
-    render(){
-        return(
-            <div>
-                {Greeting},
-                <h1>Hello {this.props.myname}</h1>
-            </div>
-        );
-    }
-}
+//TODO: Add test case later.
 
 ReactDOM.render(
     <First myname="Albert!"/>,
