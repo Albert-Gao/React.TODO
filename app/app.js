9b4a148c8e41bc1c832a3f6a1f80702d895bc499
Albert Gao <albertgaohy@gmail.com>
2017-04-23 00:24:09 +1200

    Add code for HMR
    Signed-off-by: Albert Gao <albertgaohy@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {First} from './Components'
+import {AppContainer} from 'react-hot-loader'
 
 //TODO: Add test case later.
 
-ReactDOM.render(
-    <First myname="Albert!"/>,
-    document.getElementById('root')
-)
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(First)
+
+// Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./Components', () => {
+        render(First)
+    });
+}
